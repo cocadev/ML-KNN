ebce259f4dd4ddefd1d10e5d53df97e308b1cb34
Cocadev <40970351+cocadev@users.noreply.github.com>
2023-03-30 03:47:17 +0800

    Update temp.py

--- temp.py
+++ temp.py
@@ -19,3 +19,36 @@ from sklearn.preprocessing import StandardScaler
 st_x = StandardScaler()    
 x_train = st_x.fit_transform(x_train)    
 x_test = st_x.transform(x_test)  
+
+
+#Fitting K-NN classifier to the training set  
+from sklearn.neighbors import KNeighborsClassifier  
+classifier= KNeighborsClassifier(n_neighbors = 5,
+metric ='minkowski', p = 2 )  
+classifier.fit(x_train, y_train)  
+
+KNeighborsClassifier(algorithm = 'auto', leaf_size = 30, metric = 'minkowski', metric_params = None, n_jobs = None, n_neighbors = 5, p = 2, 
+weights = 'uniform')  
+y_pred = classifier.predict(x_test)  
+
+#Creating the Confusion matrix  
+from sklearn.metrics import confusion_matrix  
+cm = confusion_matrix(y_test, y_pred)  
+
+#Visulaizing the trianing set result  
+from matplotlib.colors import ListedColormap  
+x_set, y_set = x_train, y_train  
+x1, x2 = nm.meshgrid(nm.arange(start = x_set[:, 0].min() - 1, stop = x_set[:, 0].max() + 1, step  = 0.01),  
+nm.arange(start = x_set[:, 1].min() - 1, stop = x_set[:, 1].max() + 1, step = 0.01))  
+mtp.contourf(x1, x2, classifier.predict(nm.array([x1.ravel(), x2.ravel()]).T).reshape(x1.shape),  
+alpha = 0.75, cmap = ListedColormap(('red','green' )))  
+mtp.xlim(x1.min(), x1.max())  
+mtp.ylim(x2.min(), x2.max())  
+for i, j in enumerate(nm.unique(y_set)):  
+   mtp.scatter(x_set[y_set == j, 0], x_set[y_set == j, 1],  
+       c = ListedColormap(('red', 'green'))(i), label = j)  
+mtp.title('K-NN Algorithm (Training set)')  
+mtp.xlabel('Age')  
+mtp.ylabel('Estimated Salary')  
+mtp.legend() 
+mtp.show()                                      
